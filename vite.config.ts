8c7a3a8dd4c76a6eab10b69cc715c00118832138
karan gerrard <karan.gerrard@gmail.com>
2026-05-13 23:04:28 +0530

    Updated vite config for dist package

--- vite.config.ts
+++ vite.config.ts
@@ -23,7 +23,8 @@ export default defineConfig({
   },
   build: {
     outDir: 'dist',
-    sourcemap: true,
+    sourcemap: false,
+    minify: 'terser',
     rollupOptions: {
       output: {
         manualChunks: {
